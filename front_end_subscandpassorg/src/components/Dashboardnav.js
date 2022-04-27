80b10a9d330be48ac7081bbe502d81a2929b8471
AnastasisGi <anastasiosgks@gmail.com>
2022-04-27 20:41:38 +0100

    working on the dashboard tabs

--- front_end_subscandpassorg/src/components/Dashboardnav.js
+++ front_end_subscandpassorg/src/components/Dashboardnav.js
@@ -1,31 +1,32 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const Dashboardnav =()=>{
 
 
+  const activepath = window.location.pathname;
+
     return (
 
-    <ul class="nav nav-pills">
-  <li class="nav-item"> 
-    <a class="nav-link active" href="#">Active</a>
-  </li>
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-    <div class="dropdown-menu">
-      <a class="dropdown-item" href="#">Action</a>
-      <a class="dropdown-item" href="#">Another action</a>
-      <a class="dropdown-item" href="#">Something else here</a>
-      <div class="dropdown-divider"></div>
-      <a class="dropdown-item" href="#">Separated link</a>
-    </div>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled" href="#">Disabled</a>
-  </li>
-</ul>
+        <ul className='nav nav-tabs'>
+
+        <li className='nav-item'>
+          <Link
+            className={`nav-link active ${activepath==="/credentials" && "active"}`}
+            to="/credentials"
+          >Credentials</Link>
+        </li>
+
+        <li className='nav-item'>
+          <Link
+            className={`nav-link active ${activepath==="/subscriptions" && "active"}`}
+            to="/subscriptions"
+          >Subscriptions</Link>
+        </li>
+
+
+
+
+        </ul>
 
 
 )
