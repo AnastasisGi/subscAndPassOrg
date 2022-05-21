bb7a1422f88eb1f6b3a9c6bf419635679f1aed17
AnastasisGi <anastasiosgks@gmail.com>
2022-05-21 07:48:45 +0100

    state for the credentials inputs are populated after the user puts values in the input

--- front_end_subscandpassorg/src/components/NewUserNameAndPassword.js
+++ front_end_subscandpassorg/src/components/NewUserNameAndPassword.js
@@ -1,84 +1,90 @@
-import React, { useState } from 'react'
+import axios from 'axios';
+import React, { useState } from 'react';
 
+const NewUserNameAndPassword = () => {
+	const [details, setDetails] = useState({
+		nameOfWebSite: '',
+		userNameOfWebSite: '',
+		passwordOfWebsite: '',
+	});
 
+	const { nameOfWebSite, userNameOfWebSite, passwordOfWebsite } = details;
 
-const NewUserNameAndPassword = () =>{
+	const handleSubmit = async (e) => {
+		// do the submit stuff
+		console.table('table', details);
+		e.preventDefault();
+		setDetails({ ...details, [e.target.name]: e.target.value });
 
-    const [details,setDetails]=useState({
-        nameOfWebSite:"",
-        userNameOfWebSite:"",
-        passwordOfWebsite:""
-    })
+		// try {
+		// 	await axios
+		// 		.post(`http://localhost:9999/registerNewCredentials`, { details })
+		// 		.then((res) => {
+		// 			console.log(res.data);
+		// 			console.table('table', res.data);
+		// 		});
+		// } catch (error) {
+		// 	console.log('error', error);
+		// }
+	};
 
-    const {nameOfWebSite,userNameOfWebSite,passwordOfWebsite} = details;
+	const NewUserNameAndPasswordForm = () => {
+		return (
+			<div className="container">
+				<form onSubmit={handleSubmit}>
+					<div className="form-group">
+						<label className="form-label"> Name of the Website</label>
+						<input
+							type="name"
+							name="nameOfWebSite"
+							className="form-control"
+							placeholder="Enter the name of the website"
+							value={nameOfWebSite}
+							onChange={handleSubmit}
+						></input>
+					</div>
+					<div className="form-group">
+						<label className="form-label"> Username of the Website</label>
+						<input
+							type="text"
+							name="userNameOfWebSite"
+							className="form-control"
+							placeholder="enter username"
+							value={userNameOfWebSite}
+							onChange={handleSubmit}
+						></input>
+					</div>
+					{/* "this is the page" */}
+					<div className="form-group">
+						<label className="form-label">Password of the website</label>
+						<input
+							type="text"
+							name="passwordOfWebsite"
+							className="form-control"
+							placeholder="enter password"
+							value={passwordOfWebsite}
+							onChange={handleSubmit}
+						></input>
+					</div>
+					<button type="submit">Upload</button>
+				</form>
+			</div>
+		);
+	};
 
-    const handleSubmit=(e)=>{
-        // do the submit stuff
-        e.preventDefault();
-
-
-      }
-
-
-    const NewUserNameAndPasswordForm = () =>{
-
-        return(
-
-              <div className='container'>
-            <form onSubmit={handleSubmit}>
-
-                    <div className='form-group'>
-            <label className='form-label'> Name of the Website</label>          
-              <input type="name"
-                className='form-control'
-                placeholder='Enter the name of the website'
-                value={nameOfWebSite}
-                onChange={(e)=>setDetails(e.target.value)}></input>
-          </div>  
-
-          <div className='form-group'>
-            <label className='form-label'> Username of the Website</label>          
-              <input type="text"
-                className='form-control'
-                placeholder='enter username'
-                value={userNameOfWebSite}
-              onChange={(e)=>setDetails(e.target.value)}></input>
-          </div>  
-
-
-          <div className='form-group'>
-            <label className='form-label'>Password of the website</label>          
-              <input type="text"
-                className='form-control'
-                placeholder='enter password'
-                value={passwordOfWebsite}
-                onChange={(e)=>(setDetails(e.target.value))}></input>
-          </div>  
-          <button type='submit'>Upload</button>
-
-            </form>
-          </div>
-        )
-    }
-
-
-
-
-    return(
-            <>        
-            <div className='col-sm'></div>
-            <div className='col-sm'>
-              <div className='container-fluid bg-light p-5 text-center'>
-                {/* {JSON.stringify(details)}                 */}
-              {NewUserNameAndPasswordForm()}       
-              Post a new username and password !!!
-              </div>
-            </div>          
-            <div className='col-sm'></div>
-        </>
-
-    )
-}
-
+	return (
+		<>
+			<div className="col-sm"></div>
+			<div className="col-sm">
+				<div className="container-fluid bg-light p-5 text-center">
+					{JSON.stringify(details)}
+					{NewUserNameAndPasswordForm()}
+					Post a new username and password !!!
+				</div>
+			</div>
+			<div className="col-sm"></div>
+		</>
+	);
+};
 
 export default NewUserNameAndPassword;
--- front_end_subscandpassorg/src/components/SignIn.js
+++ front_end_subscandpassorg/src/components/SignIn.js
@@ -1,96 +1,72 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import {useDispatch} from 'react-redux';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
+const SignIn = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
-const SignIn = () =>{
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const user = { email, password };
 
+		try {
+			await axios.post(`http://localhost:9999/login`, { user }).then((res) => {
+				if (res.data) {
+					window.localStorage.setItem('auth', JSON.stringify(res.data));
+					console.log(res.data);
 
+					dispatch({
+						type: 'LOGGED_IN_USER',
+						payload: res.data,
+					});
+					navigate('/dashboard');
+				}
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+	return (
+		<>
+			<div className="container">
+				<form onSubmit={handleSubmit}>
+					<div className="form-group mb-3">
+						<label className="form-label"> Your email</label>
+						<input
+							type="email"
+							className="form-control"
+							placeholder="enter email"
+							value={email}
+							onChange={(e) => {
+								setEmail(e.target.value);
+							}}
+						></input>
+					</div>
 
-  const dispatch=useDispatch();
-  const navigate = useNavigate();
+					<div className="form-group mb-3">
+						<label className="form-label"> Your password</label>
+						<input
+							type="password"
+							className="form-control"
+							placeholder="enter password"
+							value={password}
+							onChange={(e) => {
+								setPassword(e.target.value);
+							}}
+						></input>
+					</div>
 
-   const handleSubmit = async (e)=>{  
-     e.preventDefault();
-     const user ={email,password};
-    
-
-      try {
-        await axios.post(`http://localhost:9999/login`, {user})
-        .then(res=>{
-          
-          
-          if(res.data){ 
-            window.localStorage.setItem('auth', JSON.stringify(res.data));
-            console.log(res.data);
-
-           dispatch({
-              type: "LOGGED_IN_USER",
-              payload: res.data
-            })  
-            navigate("/dashboard");
-
-          }
-          
-          
-          
-        })
-        
-      } catch (error) {
-        console.log(error)
-      }
-      
-    }
-
-
-  return(
-      <>
-
-      <div className='container'>
-      <form onSubmit={handleSubmit} >
-
-      
-
-          <div className='form-group mb-3'>
-            <label className='form-label'> Your email</label>          
-              <input type="email"
-                className='form-control'
-                placeholder='enter email'
-               value={email}
-              onChange={(e)=>{setEmail(e.target.value)}}></input>
-          </div>  
-
-
-          <div className='form-group mb-3'>
-            <label className='form-label'> Your password</label>          
-              <input type="password"
-                className='form-control'
-                placeholder='enter password'
-               value={password}      
-                onChange={(e)=>{setPassword(e.target.value)}}></input>
-          </div>  
-
-          <button type='submit'>Sign Up</button>
-      </form>
-  
-</div>
-
-</>
-
-
-  )
-
-
-}
-
+					<button type="submit">Sign Up</button>
+				</form>
+			</div>
+		</>
+	);
+};
 
 export default SignIn;
-
-
-
-
